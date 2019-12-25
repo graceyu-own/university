3d2be78397b8e0cb7237e95dac6c8ff8185e3c02
strluck <s@strluck.com>
2019-12-25 14:00:19 +0800

    v0.2.7<SERVER-GO> | 日志色彩显示

--- falleaves-server-go/utils/logger/logger.go
+++ falleaves-server-go/utils/logger/logger.go
@@ -7,58 +7,106 @@ import (
 
 	"github.com/joho/godotenv"
 )
+
 // 日志等级
 const (
-	LevelDebug   = iota
+	LevelDebug = iota
 	LevelInfo
 	LevelWarning
 	LevelError
 	LevelFatal
 )
+
 // 日志等级 string
 const (
-	LevelDebugString   = "DEBUG"
-	LevelInfoString    = "INFO"
-	LevelWarningString = "WARNING"
-	LevelErrorString   = "ERROR"
-	LevelFatalString   = "FATAL"
+	_LevelDebugString   = "DEBUG"
+	_LevelInfoString    = "INFO"
+	_LevelWarningString = "WARNING"
+	_LevelErrorString   = "ERROR"
+	_LevelFatalString   = "FATAL"
 )
 
-var rootLogger int
+var color = []int{
+	// debug, info, warnig, error, fatal
+	0, 32, 33, 31, 31,
+}
+
+// 默认日志等级为 INFO
+var rootLogger int = LevelInfo
 
 func init() {
 	err := godotenv.Load()
 	if err != nil {
-		//log.Fatal("Error loading .env file")
+		Fatal("Error loading .env file")
 	}
-	switch level := os.Getenv("ROOTLOGGER"); level {
-		case LevelDebugString:
-			rootLogger = LevelDebug
-		case LevelInfoString:
-			rootLogger = LevelInfo
-		case LevelWarningString:
-			rootLogger = LevelWarning
-		case LevelErrorString:
-			rootLogger = LevelError
-		case LevelFatalString:
-			rootLogger = LevelFatal
+
+	level := os.Getenv("ROOTLOGGER")
+	switch level {
+	case _LevelDebugString:
+		rootLogger = LevelDebug
+		break
+	case _LevelInfoString:
+		rootLogger = LevelInfo
+		break
+	case _LevelWarningString:
+		rootLogger = LevelWarning
+		break
+	case _LevelErrorString:
+		rootLogger = LevelError
+		break
+	case _LevelFatalString:
+		rootLogger = LevelFatal
+		break
 	}
-	Debug("rootLogger:", rootLogger)
+
+	Debug("LoggerLevel: %s", level)
+}
+
+func getLevelMag(level int) string {
+	if level > LevelFatal {
+		level = LevelFatal
+	} else if level < LevelDebug {
+		level = LevelDebug
+	}
+
+	var mag string
+
+	switch level {
+	case LevelDebug:
+		mag = _LevelDebugString
+		break
+	case LevelInfo:
+		mag = _LevelInfoString
+		break
+	case LevelWarning:
+		mag = _LevelWarningString
+		break
+	case LevelError:
+		mag = _LevelErrorString
+		break
+	case LevelFatal:
+		mag = _LevelFatalString
+		break
+	}
+	return fmt.Sprintf("%c[1;0;%dm%s%c[0m", 0x1B, color[level], mag, 0x1B)
 }
 
 // Println 打印
-func Println(msg string) {
-	fmt.Printf("%s %s\n", time.Now().Format("2006-01-02 15:04:05"), msg)
+func Println(v ...interface{}) {
+	fmt.Printf("%s %s", time.Now().Format("2006-01-02 15:04:05"), fmt.Sprintln(v...))
+}
+
+func println(level int, format string, v ...interface{}) {
+	Println(fmt.Sprintf(fmt.Sprintf("[%s] %s", getLevelMag(level), format), v...))
 }
 
 // Fatal 严重错误
 // 指出每个严重的错误事件将会导致应用程序的退出
 func Fatal(format string, v ...interface{}) {
-	if LevelFatal > rootLogger {
+	if LevelFatal < rootLogger {
 		return
 	}
-	msg := fmt.Sprintf(fmt.Sprintf("[%s] %s", LevelFatalString, format), v...)
-	Println(msg)
+	println(LevelFatal, format, v...)
 	os.Exit(0)
 }
 
@@ -66,38 +114,34 @@ func Fatal(format string, v ...interface{}) {
 // 指出虽然发生错误事件，但仍然不影响系统的继续运行
 // 打印错误和异常信息，如果不想输出太多的日志，可以使用这个级别
 func Error(format string, v ...interface{}) {
-	if LevelError > rootLogger {
+	if LevelError < rootLogger {
 		return
 	}
-	msg := fmt.Sprintf(fmt.Sprintf("[%s] %s", LevelErrorString, format), v...)
-	Println(msg)
+	println(LevelError, format, v...)
 }
 
 // Warning 警告
 // 表明会出现潜在错误的情形，有些信息不是错误信息
 func Warning(format string, v ...interface{}) {
-	if LevelWarning > rootLogger {
+	if LevelWarning < rootLogger {
 		return
 	}
-	msg := fmt.Sprintf(fmt.Sprintf("[%s] %s", LevelWarningString, format), v...)
-	Println(msg)
+	println(LevelWarning, format, v...)
 }
 
 // Info 信息
 // 消息在粗粒度级别上突出强调应用程序的运行过程
 func Info(format string, v ...interface{}) {
-	if LevelInfo > rootLogger {
+	if LevelInfo < rootLogger {
 		return
 	}
-	msg := fmt.Sprintf(fmt.Sprintf("[%s] %s", LevelInfoString, format), v...)
-	Println(msg)
+	println(LevelInfo, format, v...)
 }
 
 // Debug 调试
 func Debug(format string, v ...interface{}) {
-	if LevelDebug > rootLogger {
+	if LevelDebug < rootLogger {
 		return
 	}
-	msg := fmt.Sprintf(fmt.Sprintf("[%s] %s", LevelDebugString, format), v...)
-	Println(msg)
+	println(LevelDebug, format, v...)
 }
